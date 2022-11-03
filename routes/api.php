d5d85669ff41c778670bdc5e44ee1d499bcb8aa5
Marcos Gonçalves <marcoslopesg7@gmail.com>
2022-11-03 00:54:44 -0300

    Aplicando 'apiResources'

--- routes/api.php
+++ routes/api.php
@@ -17,23 +17,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/testamentos', [TestamentoController::class, 'index']);
-Route::get('/testamento/{id}', [TestamentoController::class, 'show']);
-Route::put('/testamento/{id}', [TestamentoController::class, 'update']);
-Route::delete('/testamento/{id}', [TestamentoController::class, 'destroy']);
-Route::post('/testamento', [TestamentoController::class, 'store']);
-
-Route::get('/livros', [LivroController::class, 'index']);
-Route::get('/livro/{id}', [LivroController::class, 'show']);
-Route::put('/livro/{id}', [LivroController::class, 'update']);
-Route::delete('/livro/{id}', [LivroController::class, 'destroy']);
-Route::post('/livro', [LivroController::class, 'store']);
-
-Route::get('/versiculos', [VersiculoController::class, 'index']);
-Route::get('/versiculo/{id}', [VersiculoController::class, 'show']);
-Route::put('/versiculo/{id}', [VersiculoController::class, 'update']);
-Route::delete('/versiculo/{id}', [VersiculoController::class, 'destroy']);
-Route::post('/versiculo', [VersiculoController::class, 'store']);
+Route::apiResources([
+    'testamento' => TestamentoController::class,
+    'livro' => LivroController::class,
+    'versiculo' =>  VersiculoController::class
+]);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
